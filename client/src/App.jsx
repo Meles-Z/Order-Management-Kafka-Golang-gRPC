fe161bb2820f5d9ceea7431ed907a381467c91f0
Meles Zawude <meles.zawdie@gmail.com>
2024-06-19 14:52:56 +0300

    some change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Header from './components/layout/Header'
+import Homepage from './pages/HomePage'
 
 function App() {
  
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Header/>
+     <Homepage/>
     </>
   )
 }
