e4916b8c39d8e35833ee2454250d577d35fcdabe
Meles-Z <meles.zawdie@gmail.com>
2025-05-29 16:47:41 +0300

    database connection and ssetup are done

---
+++ order-service/cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/order_management/order_service/internal/database"
+)
+
+func main() {
+	fmt.Println("Starting Order Service...")
+
+	_, err := database.InitDb()
+	if err != nil {
+		log.Fatalf("Database initialization failed: %v", err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	ech := echo.New()
+	ech.GET("/", func(c echo.Context) error {
+		return c.String(200, "Order Service is running.")
+	})
+
+	ech.Logger.Fatal(ech.Start(":" + port))
+}
---
+++ order-service/internal/database/db.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDb() (*gorm.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		port = "5432"
+	}
+	user := os.Getenv("DB_USER")
+	name := os.Getenv("DB_NAME")
+	password := os.Getenv("DB_PASSWORD")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
+		host, port, user, name, password)
+
+	conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	DB = conn
+	fmt.Println("✅ Connected to Postgres successfully!")
+	return conn, nil
+}
