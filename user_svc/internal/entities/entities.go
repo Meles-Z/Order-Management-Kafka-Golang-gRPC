192bf49aec677b66c5ab4073de1f942b8a38ccea
Meles-Z <meles.zawdie@gmail.com>
2025-05-30 22:21:47 +0300

    add repository and entities

---
+++ user_svc/internal/entities/entities.go
@@ -0,0 +1,30 @@
+package entities
+
+import(
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+type User struct {
+	Model
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phoneNumber"`
+	Password    string `json:"password"`
+	Address     string `json:"address"`
+}
+
+
+type Model struct {
+	ID        string         `json:"id" gorm:"primaryKey"`
+	CreatedAt time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+}
+
+// BeforeCreate is a GORM hook that sets a UUID before inserting a record.
+func (model *Model) BeforeCreate(tx *gorm.DB) (err error) {
+	model.ID = uuid.NewString()
+	return
+}
---
+++ user_svc/internal/repository/user.go
@@ -0,0 +1,66 @@
+package repository
+
+import (
+	"github.com/order_management/user_svc/internal/entities"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{
+		DB: db,
+	}
+}
+
+func (repo *UserRepository) CreateUser(user *entities.User) (*entities.User, error) {
+	err := repo.DB.Create(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (repo *UserRepository) GetUsers() ([]entities.User, error) {
+	var users []entities.User
+	err := repo.DB.Find(&users).Error
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (repo *UserRepository) FindUserById(id string) (*entities.User, error) {
+	var user entities.User
+	err := repo.DB.Where("id=?", id).Take(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (repo *UserRepository) UpdateUser(user *entities.User) (*entities.User, error) {
+	var existingUser entities.User
+	err := repo.DB.Model(&user).Where("id=?", user.ID).Updates(entities.User{
+		Name:        user.Name,
+		Email:       user.Email,
+		PhoneNumber: user.PhoneNumber,
+		Password:    user.Password,
+		Address:     user.Address,
+	}).Scan(&existingUser).Error
+	if err != nil {
+		return nil, err
+	}
+	return &existingUser, err
+}
+
+func (repo *UserRepository) DeleteUser(id string) error {
+	var user entities.User
+	err := repo.DB.Where("id=?", id).Delete(&user).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
