1024e66a4d0a0dc0942398cd372e1e867bf42285
Meles-Z <meles.zawdie@gmail.com>
2025-06-11 15:00:18 +0300

    feat: add inventory service

---
+++ inventory-service/internal/service/inventory.go
@@ -0,0 +1,62 @@
+package service
+
+import (
+	"github.com/order_management/iventory_service/internal/entities"
+	"github.com/order_management/iventory_service/internal/repository"
+	"github.com/order_management/iventory_service/pkg/logger"
+)
+
+type Service struct {
+	repo *repository.Repostory
+}
+
+func NewServices(repo *repository.Repostory) *Service {
+	return &Service{
+		repo: repo,
+	}
+}
+
+func (s *Service) DeleteInventory(inventory *entities.Inventory) (*entities.Inventory, error) {
+	inv, err := s.repo.CreateEventory(inventory)
+	if err != nil {
+		logger.Error("error to create invetory", "error", err)
+		return nil, err
+	}
+	return inv, nil
+}
+
+func (r *Service) GetInventory() ([]entities.Inventory, error) {
+	inv, err := r.repo.GetInventory()
+	if err != nil {
+		logger.Error("error to find invetory", "error", err)
+		return nil, err
+	}
+	return inv, nil
+}
+
+func (r *Service) FindInvetoryById(id string) (*entities.Inventory, error) {
+	inv, err := r.repo.FindInvetoryById(id)
+	if err != nil {
+		logger.Error("error to find invetory", "error", err)
+		return nil, err
+	}
+	return inv, nil
+}
+
+func (r *Service) UpdateInvitories(env *entities.Inventory) (*entities.Inventory, error) {
+	inv, err := r.repo.UpdateInvitories(env)
+	if err != nil {
+		logger.Error("error to update invetory", "error", err)
+		return nil, err
+	}
+	return inv, nil
+}
+
+func (r *Service) DeleteInvitory(id string) error {
+	err := r.repo.DeleteInventory(id)
+	if err != nil {
+		logger.Error("error to update invetory", "error", err)
+		return err
+	}
+	return nil
+}
